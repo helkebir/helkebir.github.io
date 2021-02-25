1ff8579ade61dc4f6a3a0caeba7308dfd32d945c
Hamza El-Kebir <ha.elkebir@gmail.com>
2021-02-24 20:56:36 -0600

    Update website

---
+++ ja.search.min.b56a1c66b1b1b9891cd37833fcd8450dd4a26a68702be5bc025010242a2da26a.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/ja.search-data.min.8ada05e03645c3f8eec4ca8272cf06bf07e1360a05413155020de9dd08c29b1f.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
