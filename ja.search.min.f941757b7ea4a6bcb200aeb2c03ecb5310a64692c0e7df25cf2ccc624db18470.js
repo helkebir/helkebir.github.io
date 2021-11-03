e4c2c3a72ad102981cb6a5ad16af43ede7180e31
Hamza El-Kebir <ha.elkebir@gmail.com>
2021-11-03 08:41:59 -0500

    Add MESAS manuscript

---
+++ ja.search.min.f941757b7ea4a6bcb200aeb2c03ecb5310a64692c0e7df25cf2ccc624db18470.js
@@ -0,0 +1,5 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/ja.search-data.min.110c2c8a36c4bd30af16d740922df2fc808cfe7beb6c41175b8191f636f95804.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
